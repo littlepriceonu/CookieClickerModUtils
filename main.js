473d2ef79b7769c0f08038665f6191ec21b29d99
Little <63677850+littlepriceonu@users.noreply.github.com>
2021-12-23 13:56:01 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-window.ModUtils = {}
-ModUtils.GameLoadCallback = []
-ModUtils.addGameLoadCallback = (callback) => {ModUtils.GameLoadCallback.push(callback)}
-setTimeout(() => {
-    window.onload = () => {
-    	
-    	if (Game.ready) {		
-    		console.log('[=== Cheated cookies taste AMAZING! ===]');
-    		Game.Load();
-    	}
-    }
-}, 10)
+    window.ModUtils = {};
+    var ModUtils = window.ModUtils;
+    var Game = window.Game
+    ModUtils.GameLoadCallback = [];
+    ModUtils.addGameLoadCallback = (callback) => {ModUtils.GameLoadCallback.push(callback)};
+    setTimeout(() => {
+        window.onload = () => {
+            if (!Game.ready) {
+                if (window.top!=window.self) Game.ErrorFrame();
+                else {
+                    console.log('[=== Cheated cookies taste AMAZING! ===]');
+                    for (const callback in ModUtils.GameLoadCallBack) {
+                        callback();
+                    }
+                    Game.Load();
+                }
+            }
+        }
+    }, 10)
